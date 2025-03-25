d8d059edd072b2e5324db639a18e8aa3a5a5115a
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2025-03-25 16:25:15 +0100

    Update raylib_ext.c.v

--- raylib_ext.c.v
+++ raylib_ext.c.v
@@ -14,6 +14,9 @@ module raylib
 #flag windows -lgdi32 -lwinmm
 
 // test	
+// if android{
+        // #include "android-ext/native_app_glue/android_native_app_glue.c"
+// }
 #flag android -llog -landroid -lEGL -lGLESv2 -lOpenSLES -lm
 #flag android -DPLATFORM_ANDROID -DGRAPHICS_API_OPENGL_ES2 -Iexternal/android/native_app_glue -Wno-implicit-const-int-float-conversion
 
